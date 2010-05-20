616190fdaad371f42e5498faa4e18e325cb0775b
Darren Shafae <dshafae@papercheck.com>
2010-05-20 14:06:37 -0700

    There was another error on page 33 of JavaScript: The Good parts. I am not sure what the difference between 'ceil' and 'ceiling' are, but I have made a note of this mistake in augmenting_types.js. I also see that trim does not exist in JavaScript. You can use regular expressions to make you own trip function.

--- chapter_4/augmenting_types.js
+++ chapter_4/augmenting_types.js
@@ -5,8 +5,18 @@ Function.prototype.method = function (name, func) {
     return this;
 };
 
+// On Page 33, this is represented by Math[this < 0 ? 'ceiling' : 'floor'](this);
+// which is wrong..
+
 Number.method('integer', function () {
-    return Math[this < 0 ? 'ceiling' : 'floor'](this);
+    return Math[this < 0 ? 'ceil' : 'floor'](this);
 });
 
 document.writeln((-10 / 3).integer()); //3
+document.writeln((-10 / 3)); //3
+
+String.method('trim', function() {
+   return this.replace(/^\s+|\s+$/g, '');
+});
+
+document.writeln('"' + "   neat     ".trim() + '"');

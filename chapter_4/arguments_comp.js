9b1ede19633d15e43136847539c01381a934ccb0
Darren Shafae <dshafae@papercheck.com>
2010-05-20 13:39:35 -0700

    Playing around with the examples in Chapter 4 of "JavaScript: The Good Parts." Ran an experiment by minimizing the JavaScript in argument_comp.js vs. arguments.js. It is amazing that minimizing JavaScript can save 67%!

--- chapter_4/arguments_comp.js
+++ chapter_4/arguments_comp.js
@@ -1,5 +1,5 @@
 var sum=function(){var i,sum=0;for(i=0;i<arguments.length;i+=1){sum+=arguments[i];}
 return sum;};document.writeln(sum(4,8,15,16,23,42));var add=function(a,b){if(typeof a!=='number'||typeof b!=='number'){throw{name:'TypeError',message:'add needs numbers'};}
 return a+b;}
-var try_it=function(){try{add("seven");}catch(e){document.writeln(e.name+': '+e.message);}}
+var try_it=function(){try{add("Seven");}catch(e){document.writeln(e.name+': '+e.message);}}
 try_it();document.writeln(add(4,2));
